df1ec8b839c6b31ca58347eaaf9c4feee75d556c
mustafa inaya <moose.inaya@icloud.com>
2019-04-19 19:04:44 -0700

    UPDATE.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const express = require('express');
+
+const projectRoute = require('./routes/projectRoute');
+const actionRoute = require('./routes/actionRoute');
+
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/projects', projectRoute);
+server.use('/api/actions', actionRoute);
+
+server.get('/', (req, res) => {
+	res.send(`Hello And Welcome to RDBMS`);
+});
+
 const port = process.env.PORT || 9999;
 
 server.listen(port, () => {
